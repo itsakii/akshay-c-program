91be163f8dd6786e6ff765951260e7e1047d82da
itsakii <152154246+itsakii@users.noreply.github.com>
2023-11-27 13:30:20 +0530

    Add files via upload

---
+++ pirto.cpp
@@ -0,0 +1,84 @@
+#include<iostream> 
+using namespace std;
+
+char toLowerCase(char ch) {
+    if(ch >='a' && ch <='z')
+        return ch;
+    else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool checkPalindrome(char a[], int n) {
+    int s = 0;
+    int e = n-1;
+
+    while(s<=e) {
+        if(toLowerCase( a[s] ) != toLowerCase( a[e] ) )
+        {
+            return 0;       
+        }
+        else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+
+void reverse(char name[], int n) {
+    int s=0;
+    int e = n-1;
+
+    while(s<e) {
+        swap(name[s++], name[e--]);
+    }
+}
+
+int getLength(char name[]) {
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++) {
+        count++;
+    }
+
+    return count;
+}
+
+char getMaxOccCharacter(string s) {
+
+    int arr[26] = {0};
+
+
+ 
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+     
+        int number = 0;
+        number = ch - 'a';
+        arr[number]++;
+    }
+
+ 
+    int maxi = -1, ans = 0;
+    for(int i=0;i<26; i++) {
+        if(maxi < arr[i]) {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    
+
+    return 'a'+ans;
+
+}
+
+int main( ) {
+
+   string s;
+   cin >> s;
+
+    cout << getMaxOccCharacter(s) << endl;
+
+    return 0;
+}
