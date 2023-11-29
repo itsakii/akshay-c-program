201f3f36438a5de6b7b9dbd848c90c644cf98174
itsakii <152154246+itsakii@users.noreply.github.com>
2023-11-29 18:46:56 +0530

    Add files via upload

---
+++ 2dArrayIntro.cpp
@@ -0,0 +1,88 @@
+#include<iostream> 
+#include <climits>
+using namespace std; 
+
+bool isPresent(int arr[][3], int target, int row, int col) {
+
+  for(int row=0; row<3; row++) {
+        for(int col=0; col<3; col++) {
+            
+            if( arr[row][col] == target) {
+                return 1;
+            }
+            
+        }
+    }
+    return 0;
+}
+void printColSum(int arr[][3], int row, int col) {
+    cout << "Printing Sum -> " << endl;
+    for(int col=0; col<3; col++) {
+        int sum = 0;
+        for(int row=0; row<3; row++) {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+
+    cout << endl;
+}
+void printSum(int arr[][3], int row, int col) {
+    cout << "Printing Sum -> " << endl;
+    for(int row=0; row<3; row++) {
+        int sum = 0;
+        for(int col=0; col<3; col++) {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+
+    cout << endl;
+}
+
+int largestRowSum(int arr[][3], int row, int col) {
+
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+
+    for(int row=0; row<3; row++) {
+        int sum = 0;
+        for(int col=0; col<3; col++) {
+            sum += arr[row][col];
+        }
+
+        if(sum > maxi ) {
+            maxi = sum;
+            rowIndex = row;
+        }
+       
+    }
+
+    cout << "the maximum sum is " << maxi << endl;
+    return rowIndex;
+}
+
+int main() {
+    int arr[3][3];
+
+    cout << "Enter the elements " <<  endl;
+    
+    for(int row=0; row<3; row++) {
+        for(int col=0; col<3; col++) {
+            cin >> arr[row][col];
+        }
+    }
+    cout << "Printing the array " << endl;
+    
+       for(int row=0; row<3; row++) {
+        for(int col=0; col<3; col++) {
+            cout << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+   int ansIndex = largestRowSum(arr,3,3);
+   cout << " Max row is at index " << ansIndex << endl;
+
+    return 0;
+}
