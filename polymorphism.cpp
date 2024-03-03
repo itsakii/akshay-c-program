57875472e5699db1fafad8b9e2878ada048021fc
itsakii <152154246+itsakii@users.noreply.github.com>
2024-03-03 19:06:25 +0530

    Add files via upload

---
+++ polymorphism.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+class A {
+
+    public:
+    void sayHello() {
+        cout << "Hello Love Babbar" << endl;
+    }
+    
+    int sayHello(char name) {
+        cout << "Hello Love Babbar" << endl;
+        return 1;
+    }
+
+    void sayHello(string name) {
+        cout << "Hello " << name  << endl;
+    }
+
+};
+
+class B {
+    public:
+    int a;
+    int b;
+
+    public: 
+    int add() {
+        return a+b;
+    }
+
+    void operator+ (B &obj) {
+
+       cout << "Hello Babbar" << endl;
+    }
+
+    void operator() () {
+        cout << "main Bracket hu " << this->a << endl;
+    }
+
+};
+
+class Animal {
+    public:
+    void speak() {
+        cout << "Speaking "<< endl;
+    }
+};
+
+class Dog: public Animal {
+
+    public:
+    void speak() {
+        cout << "Barking " << endl;
+    }
+
+
+};
+
+int main() {
+
+    Dog obj;
+    obj.speak();
+
+
+
+    B obj1, obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+    obj1();
+
+
+    return 0;
+}
